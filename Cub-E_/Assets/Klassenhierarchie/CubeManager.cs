e498b7e22092871cce468e3d622a9facc4877b30
Cube-E-Conny <40527283+Cube-E-Conny@users.noreply.github.com>
2018-09-11 14:46:13 +0200

    Klasse CubeManager aktualisiert

--- Cub-E_/Assets/Klassenhierarchie/CubeManager.cs
+++ Cub-E_/Assets/Klassenhierarchie/CubeManager.cs
@@ -2,15 +2,107 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+   In dieser Klasse wird alles rund um den Cube programmiert.
+   - Cube wird erzeugt 
+   - das Rotationsverhalten gesteuert
+   - das Rotationsverhalten definiert
+ */
+
+
 public class CubeManager : MonoBehaviour {
+	
+	private int drehrichtung;
+    public Vector2 textureSpeed;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+	private void CubeDrehen (){
+
+        //System.Random zufall = new System.Random();
+        // Initialisieren von drehrichtung mit Zufallszahlen zwischen und einschließlich 0 und 3 (vier Möglichkeiten!)
+        //drehrichtung = zufall.Next( 0,  4);
+        int rnd = Random.Range(0, 4);
+		
+		//Auswahl der Rotationsrichtung mit SwitchCase
+		switch (drehrichtung){
+		
+			// wenn drehrichtung == 0, 
+					//dann RotationHorizontal(float richtung);
+			case 0: RotationHorizontal( 90);
+					break;
+				
+				
+			// wenn drehrichtung == 1, 
+				//dann RotationHorizontal(float - richtung);
+			case 1: RotationHorizontal (-90);
+					break;
+			
+			// wenn drehrichtung == 2, 
+				//dann RotationVertikal(float richtung);
+			case 2: RotationVertikal ( 90);
+					break;
+					
+			// wenn drehrichtung == 3, 
+				//dann RotationVertikal(float - richtung);
+			case 3: RotationVertikal (-90);
+					break;
+
+            default: break;
+        }
+				
+	}
+	
+	//Rotationsbewegung nach oben oder unten programmieren
+	void RotationVertikal (float richtung){
+	
+		float geschwindigkeit = 1f;
+	
+		// globale Variable aktivModus auf false, zur Organisation von SpielerInput
+		SpielUeberwachung.aktivModus = false;
+	
+		// Cube dreht sich vertikal um x Achse um übergebenen Winkel 
+		transform.Rotate(new Vector3(richtung,0,0), geschwindigkeit);
+		
+		//alternativ???
+		//transform.eulerAngles += new Vector3(0f, 0f, richtung);
+		
+		// Funktionsaufruf Feldwechsel() der Klasse Hindernisse
+		//geht noch nicht 	Hindernisse.Feldwechsel();
+		
+		// globale Variable aktivModus auf true, zur Organisation von SpielerInput
+		SpielUeberwachung.aktivModus = true;
+		
+	}
+	
+	//Rotationsbewegung nach links oder rechts programmieren
+	void RotationHorizontal (float richtung){
+	
+		//float geschwindigkeit = 100f;
+	
+		// globale Variable aktivModus auf false, zur Organisation von SpielerInput
+		SpielUeberwachung.aktivModus = false;
+	
+		// Cube dreht sich vertikal um y Achse um übergebenen Winkel 
+		transform.Rotate(new Vector3(0,richtung,0));
+		
+		// Funktionsaufruf Feldwechsel() der Klasse Hindernisse
+		// geht noch nicht 	Hindernisse.Feldwechsel();
+		
+		// globale Variable aktivModus auf true, zur Organisation von SpielerInput
+		SpielUeberwachung.aktivModus = true;
+		
+	}
+	
 	// Update is called once per frame
 	void Update () {
 		
+		//Bewegung des Materials
+		Material mat = GetComponent<Renderer>().material;
+        mat.SetTextureOffset("_MainTex",  mat.GetTextureOffset("_MainTex") + textureSpeed);
+
 	}
 }
--- Cub-E_/Assets/Klassenhierarchie/SpielManager.cs
+++ Cub-E_/Assets/Klassenhierarchie/SpielManager.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+   In dieser Klasse werden die organisatorischen Dinge sozusagen aus dem Hintergrund  geregelt.
+   - globale Variablen zur Verwaltung des Spiels generiert und verwaltet
+   - die Größen aus dem Menü werden implementiert und verwaltet
+   - das Menü selbst wird gesteuert mithilfe von globalen Variablen
+   - das Spiel wird beendet und gestartet
+   - die Spielzeit wird hier in einer Variable gespeichert und verwaltet
+
+*/
+
+
 public class SpielManager : MonoBehaviour {
+	
+	public bool aktivModus;
 
 	// Use this for initialization
 	void Start () {
+		
+		aktivModus = false;
 		
 	}
 	
