04b81f937df95a5d424bccaa4e9bf93040aceeea
Cube-E-Conny <40527283+Cube-E-Conny@users.noreply.github.com>
2018-09-11 14:29:52 +0200

    SpielerManager Springen ergänzt

--- Cub-E_/Assets/Klassenhierarchie/SpielerManager.cs
+++ Cub-E_/Assets/Klassenhierarchie/SpielerManager.cs
@@ -15,9 +15,16 @@ public class SpielerManager : MonoBehaviour {
 	
 	//Vektor zur Speicherung des Nullvektors
 	private Vector3 targetPos = Vector3.zero;
+	public float step;
+    public float smoothing;
+    private float animation;
 
 	// Use this for initialization
 	void Start () {
+		
+	}
+	
+	void Abtauchen (){
 		
 	}
 	
@@ -34,6 +41,20 @@ public class SpielerManager : MonoBehaviour {
 		targetPos = transform.right * step;
 	}
 	
+	//Spieler bei Tastenbedienung W Bewegung in einer Parabel über das Hindernis
+	if  (Input.GetKeyDown( KeyCode.W )){
+		
+		animation += Time.deltaTime;
+		
+		animation = animation % 3f;
+		
+		Vector3 startposition = transform.position;
+		
+		transform.position = MathParabola.Parabola(transform.position, new Vector3 (transform.position.x, 4.9f, -4f), 20f, animation/3f);
+		
+	}
+	
+	//Bewegungen nach links und rechts abdämpfen mit smoothing
 	transform.position += targetPos * smoothing;
     targetPos -= targetPos * smoothing;
 	}
