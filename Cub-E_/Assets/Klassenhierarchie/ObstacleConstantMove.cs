d1ba99c9ea8d9aa433a4738aef125b4a22931707
Cube-E-Leonie <40502752+Cube-E-Leonie@users.noreply.github.com>
2018-09-11 14:44:58 +0200

    Bewegung Hindernisse
    Skript ist drin, funktioniert aber noch nicht

---
+++ Cub-E_/Assets/Klassenhierarchie/ObstacleConstantMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleConstantMove : MonoBehaviour
+{
+
+    gameManager GameManager;
+
+    void Start()
+    {
+        GameObject gameController = GameObject.FindGameObjectWithTag("GameController"); // SpawnerManager ist getagged
+        GameManager = gameController.GetComponent<gameManager>(); // addresiert an Eigenschaften des anderen Skripts
+    }
+
+
+    void Update()
+    {
+        transform.Translate(GameManager.moveVector * GameManager.moveSpeed * Time.deltaTime); // fuer die Bewegung zustaendig
+    }
+}
---
+++ Cub-E_/Assets/Klassenhierarchie/gameManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+
+    public float moveSpeed = 1.0f; // Kann manuel in Unity nich veraendert werden
+    public Vector3 moveVector; // Bewegungsrichtung
+}
