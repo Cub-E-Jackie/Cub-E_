0496cc71b0d7e15144f129ef1a47f1f7232e365d
Cube-E-Conny <40527283+Cube-E-Conny@users.noreply.github.com>
2018-09-11 14:21:53 +0200

    SpielerManager links und rechts Bewegung

--- Cub-E_/Assets/Klassenhierarchie/SpielerManager.cs
+++ Cub-E_/Assets/Klassenhierarchie/SpielerManager.cs
@@ -2,7 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpielerInput : MonoBehaviour {
+/*
+  In dieser Klasse wird die Steuerung des Spiels definiert und verwaltet.
+  - die Steuerung WÄHREND des Levels
+  - die Steuerung des Spielers in horizontaler Richtung, nach links und rechts
+  - abhängig von Spielmodus
+  - Kollision abfangen und entsprechend andere Funktionen aufrufen
+  
+*/
+
+public class SpielerManager : MonoBehaviour {
+	
+	//Vektor zur Speicherung des Nullvektors
+	private Vector3 targetPos = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
@@ -12,5 +24,17 @@ public class SpielerInput : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	// Spieler bei Tastenbedienung A Bewegung nach links
+	if( Input.GetKeyDown( KeyCode.A )){
+		targetPos = -(transform.right * step);
+	}
+	
+	//Spieler bei Tastenbedienung D Bewegung nach rechts
+	if( Input.GetKeyDown( KeyCode.D )){
+		targetPos = transform.right * step;
+	}
+	
+	transform.position += targetPos * smoothing;
+    targetPos -= targetPos * smoothing;
 	}
 }
