fef62934af4bcf9f324229587025efbf9a7c6dd6
Cube-E-Conny <40527283+Cube-E-Conny@users.noreply.github.com>
2018-09-14 12:16:07 +0200

    Skript für Kamera

---
+++ Cub-E_/Assets/Klassenhierarchie/KameraBewegen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraBewegen : MonoBehaviour {
+
+	public GameObject player; 
+	float abstandXZ = 6f;
+	float hoeheY = 4f;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Quaternion fahrzeugRotationY = new Quaternion();
+		
+		fahrzeugRotationY.eulerAngles = new Vector3 (0, player.transform.eulerAngles.y, 0);
+		
+		Vector3 abstandHinterFahrzeug = fahrzeugRotationY * new Vector3 (0, 0, abstandXZ);
+		
+		transform.position = player.transform.position - abstandHinterFahrzeug;
+		transform.position = new Vector3 (transform.position.x, transform.position.y + hoeheY, transform.position.z);
+		transform.LookAt(player.transform);
+		
+	}
+}
+
