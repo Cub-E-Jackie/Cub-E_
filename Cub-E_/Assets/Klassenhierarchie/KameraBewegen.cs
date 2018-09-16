f0d47dc10eb726d28bb2d7c3829f1b07a4a29dc4
Cube-E-Conny <40527283+Cube-E-Conny@users.noreply.github.com>
2018-09-16 11:05:14 +0200

    Viel an Kamera gearbeitet, Planes für Kamera eingefügt, noch nicht bearbeitet
    Zwei neue Kameraklassen erstellt, um Kreisbahn um Drehmoment mit Kamera zu machen, SookAt auf Spieler und Drehmoment
    Skript vom Spawner deaktiviert, um daran arbeiten zu können
    Planes erzeugt für Kollision und Kameradrehungen

--- Cub-E_/Assets/Klassenhierarchie/KameraBewegen.cs
+++ Cub-E_/Assets/Klassenhierarchie/KameraBewegen.cs
@@ -5,23 +5,6 @@ using UnityEngine;
 public class KameraBewegen : MonoBehaviour {
 
 	public GameObject entePlayer; 
-	public Vector3 abstandHinterEnte;
-	public float abstandXZ = 6f;
-	public float hoeheY = 4f;
 	
-	// Update is called once per frame
-	void Update () {
-		
-		Quaternion fahrzeugRotationY = new Quaternion();
-		
-		fahrzeugRotationY.eulerAngles = new Vector3 (0, entePlayer.transform.eulerAngles.y, 0);
-		
-		abstandHinterEnte = fahrzeugRotationY * new Vector3 (0, 0, abstandXZ);
-		
-		transform.position = entePlayer.transform.position - abstandHinterEnte;
-		transform.position = new Vector3 (transform.position.x, transform.position.y + hoeheY, transform.position.z);
-		transform.LookAt(entePlayer.transform);
-		
-	}
 }
 
---
+++ Cub-E_/Assets/Klassenhierarchie/KameraSkriptKreis.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraSkriptKreis : MonoBehaviour {
+	
+	public GameObject spieler;
+	public Vector3 abstandHinterEnte;
+	public float abstandXZ = 6f;
+	public float hoeheY = 4f;
+	
+		
+		
+	// Update is called once per frame
+	void Update () {
+		
+		Quaternion fahrzeugRotationY = new Quaternion();
+		
+		fahrzeugRotationY.eulerAngles = new Vector3 (0, spieler.transform.eulerAngles.y, 0);
+		
+		abstandHinterEnte = fahrzeugRotationY * new Vector3 (0, 0, abstandXZ);
+		
+		transform.position = spieler.transform.position - abstandHinterEnte;
+		transform.position = new Vector3 (transform.position.x, transform.position.y + hoeheY, transform.position.z);
+		transform.LookAt(spieler.transform);
+		
+	}
+}
