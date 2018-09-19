909f4e06248dee64be360f4568ba71c38ba0dc90
Cube-E-Leonie <40502752+Cube-E-Leonie@users.noreply.github.com>
2018-09-19 14:28:53 +0200

    Überarbeitung Audio und Enten Spawner

--- Cub-E_/Assets/Klassenhierarchie/Spawner.cs
+++ Cub-E_/Assets/Klassenhierarchie/Spawner.cs
@@ -34,14 +34,14 @@ public class Spawner : MonoBehaviour
 	public float spawnWoodFrequency = 0.1f;
 	public float spwanStoneFrequency = 0.1f;
 	public GameObject root;
-	private float timer = 1f;
+	private float timer = 5f;
 
    // int randEnemy; //zufaelliges objekt
 	//int randDuck;
 
     void Start()
     {
-       //StartCoroutine(WaitSpawner());  // Coroutine fuer WaitForSeconds	
+        //StartCoroutine(WaitSpawner());  // Coroutine fuer WaitForSeconds	
 		//StartCoroutine(WaitSpawnerDuck());  // Coroutine fuer WaitForSeconds	
 		Update();
     }
@@ -54,8 +54,8 @@ public class Spawner : MonoBehaviour
 		GameObject duckObj3;
 		GameObject duckObj4;
 		GameObject duckObj5;
-       // spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
-		//spawnWaitDuck = Random.Range(spawnLeastWaitDuck, spawnMostWaitDuck);
+        // spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+		// spawnWaitDuck = Random.Range(spawnLeastWaitDuck, spawnMostWaitDuck);
 		timer += Time.deltaTime;
 
 	if(timer >= spawnDuckFrequency) { 
@@ -73,46 +73,47 @@ public class Spawner : MonoBehaviour
 		
 		//Instantiate zum Ausführen (welches Object, wo, rotation)
 		duckObj = Instantiate(duckPrefab, rndPos, Quaternion.identity);
-		duckObj.name = "Duck_Child";
+        duckObj.name = "Duck_Child";
 		duckObj.transform.parent = root.transform;
-		//duckObj.transform.position = transform.position;
-		duckObj.transform.eulerAngles = new Vector3(0, Random.Range(0f, 360f),0);
-        Destroy(duckObj, 5f);
+        // duckObj.transform.position = transform.position;
+        duckObj.transform.eulerAngles = new Vector3(0, Random.Range(0f, 360f), 0);
+            // FindObjectOfType<AudioManager>().Play("Ducks");
+            Destroy(duckObj, 5f);
 
         duckObj1 = Instantiate(duckPrefab1, rndPos1, Quaternion.identity);
 		duckObj1.name = "Duck_Child1";
 		duckObj1.transform.parent = root.transform;
 		//duckObj1.transform.position = transform.position;
 		duckObj1.transform.eulerAngles = new Vector3(0, Random.Range(0f, 360f),0);
-        Destroy(duckObj1, 5f);
+            Destroy(duckObj1, 5f);
 
         duckObj2 = Instantiate(duckPrefab2, rndPos2, Quaternion.identity);
 		duckObj2.name = "Duck_Child1";
 		duckObj2.transform.parent = root.transform;
 		//duckObj2.transform.position = transform.position;
 		duckObj2.transform.eulerAngles = new Vector3(Random.Range(0f, 360f), 0, 0);
-        Destroy(duckObj2, 5f);
+            Destroy(duckObj2, 5f);
 
         duckObj3 = Instantiate(duckPrefab3, rndPos3, Quaternion.identity);
 		duckObj3.name = "Duck_Child2";
 		duckObj3.transform.parent = root.transform;
 		//duckObj3.transform.position = transform.position;
 		duckObj3.transform.eulerAngles = new Vector3(Random.Range(0f, 360f),0, 0);
-        Destroy(duckObj3, 5f);
+            Destroy(duckObj3, 5f);
 
         duckObj4 = Instantiate(duckPrefab4, rndPos4, Quaternion.identity);
 		duckObj4.name = "Duck_Child4";
 		duckObj4.transform.parent = root.transform;
 		//duckObj4.transform.position = transform.position;
 		duckObj4.transform.eulerAngles = new Vector3( 0, 0, Random.Range(0f, 360f));
-        Destroy(duckObj4, 5f);
+            Destroy(duckObj4, 5f);
 
         duckObj5 = Instantiate(duckPrefab5, rndPos5, Quaternion.identity);
 		duckObj5.name = "Duck_Child5";
 		duckObj5.transform.parent = root.transform;
 		//duckObj5.transform.position = transform.position;
 		duckObj5.transform.eulerAngles = new Vector3( 0, 0, Random.Range(0f, 360f));
-        Destroy(duckObj5, 5f);
+            Destroy(duckObj5, 5f);
 
 
 
