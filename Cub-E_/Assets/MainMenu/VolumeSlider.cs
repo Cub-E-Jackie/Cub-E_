432c71dd17b4bca8393e6879e52c0b53f0ea28e1
Cube-E-Leonie <40502752+Cube-E-Leonie@users.noreply.github.com>
2018-09-22 12:09:45 +0200

    Audio Slider Menü & Clicks
    Clicks nicht vollständig

---
+++ Cub-E_/Assets/MainMenu/VolumeSlider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System;
+
+public class VolumeSlider : MonoBehaviour
+{
+
+    // Referenz zum Audio Source
+    private AudioSource audioSrc;
+
+    // Wert, der durch den Slider angepasst wird
+    private float musicVolume = 1f;
+
+
+    void Start()
+    {
+
+        //Zuordnung von Audio Source zur besseren Kontrolle
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+        // musicVolume wird zum volume von Audio Source gleichgesetzt
+        audioSrc.volume = musicVolume;
+    }
+
+    // diese Methode wird vom Slider aufgerufen
+    // uebernimmt vol-Wert und setzt ihn als musicValue
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
