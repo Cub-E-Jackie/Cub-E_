9b2b05b853ac7d5e844772b974bafbdfb0e6ede5
Cube-E-Conny <40527283+Cube-E-Conny@users.noreply.github.com>
2018-09-23 20:42:41 +0200

    Blockierung Bugs

--- Cub-E_/Assets/Klassenhierarchie/SpielerInput.cs
+++ Cub-E_/Assets/Klassenhierarchie/SpielerInput.cs
@@ -2,40 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// noch nicht funktionsfähig, muss noch angepassz werden
-
 /*
   In dieser Klasse wird die Steuerung des Spiels definiert und verwaltet.
-  - die Steuerung WÄHREND des Levels
-  - die Steuerung des Spielers in horizontaler Richtung, nach links und rechts
-  - abhängig von Spielmodus
-  - Kollision abfangen und entsprechend andere Funktionen aufrufen
-  - LERP der Ente
-  
+  - die Steuerung des Players: A - Bewegung nach links
+  							   D - Bewegung nach rechts
+  						   Space - Sprungbewegung (begrenzt!)
+  - OnTriggerEnter() für die Planes auf den Kanten und den damit erzeugten Kameraanimationen
+  - OnTriggerStay() für die Planes jeweils links und rechts für die Begrenzung der Steuerung  
 */
 
 public class SpielerInput	: MonoBehaviour {
 
 
-	public Rigidbody drehKin;
-
     //Vektor zur Speicherung des Nullvektors
     private Vector3 targetPos = Vector3.zero;
-	public float step;
+	// Bewegungen nach links und rechts weicher machen/ abdämpfen
+    public float step;
     public float smoothing;
 
 
     public GameObject duckPref;
     public GameObject worldCube;
-    
+    public GameObject duck;
     public GameObject test;
     public float turnSpeed = 50f;
+    
    
-    
-    public static  float winkelGesamt;
-        
-    public GameObject planeObenVorne;
-    
+    //Variablen zum Aufruf der Kameraanimationen der jeweiligen Kameraklasse 
+    //oder der Auslösung der Blockierung der Bewegung
     public static bool drehOV;
     public static bool drehOH;
     public static bool drehOL;
@@ -49,74 +43,60 @@ public class SpielerInput	: MonoBehaviour {
     public static bool drehHR;
     public static bool drehHL;
     
+    //Variable für Zeitorganisation während Kameranimation (Weitergabe an jeweilige Klasse)
+    public static float bewegungZeitStart;
+    
+    //Variablen zur Speicherung der Kameraobjekte (für Speicherung der Rotationsdaten)
     public Camera kameraOben;
 	public Camera kameraUnten;
-	public Camera kameraLinks;
-	public Camera kameraRechts;
 	public Camera kameraHinten;
 	public Camera kameraVorne;
 	
+	//Variablen zur Speicherung der Rotationsdaten 
+	//(Typ Quaternion, damit sich die Achsen nicht übereinander verschieben)
 	public static Quaternion originalDrehOben;
 	public static Quaternion originalDrehVorne;
 	public static Quaternion originalDrehUnten;
 	public static Quaternion originalDrehHinten;
-	public static Quaternion originalDrehLinks;
-	public static Quaternion originalDrehRechts;
     
     
-    public GameObject duck;
-    
-    
+    //Variablen zur Realisierung und Begrenzung der Sprungbewegung
     public bool sprungSchalter;
- 
     public bool jumping = false;
-    
     public float jump = 0;
-    
     public float zeitSchaltuhrStart;
-    
     public float sprungSperre;
-    
     public float sprungZeitAlt;
     
+    //Variablen für die Blockierung der Link- und Rechtsbewegung
     public bool sperreLinks;
     public bool sperreRechts;
-    
-   
-    
-//    float zeitGesamt;
-//    
-//    float winkelGesamt;
-//    
-   public static float bewegungZeitStart;
-//    
-//    float zeitAnteilAlt;
-    
-  
-
-	// Use this for initialization
+     
 	void Start () {
-		
-		sperreLinks = true;
-		sperreRechts = true;
-		
-		sprungSperre = 0.3f;
-		sprungZeitAlt = 0;
-		sprungSchalter = true;
-
-        duck = Instantiate(duckPref);
-
+    	
+    	duck = Instantiate(duckPref);
         duck.name = "Duck";
         duck.transform.parent = this.transform;
         duck.transform.position = transform.position;
-        
+		
+		//zu Beginn Links- und Rechtsbewegung möglich
+		sperreLinks = true;
+		sperreRechts = true;
+		
+		//Zeit in der Sprung gesperrt ist und Variable zur Blockierung des Sprungs selber
+		//zu Beginn Sprung möglich
+		sprungSperre = 0.3f;
+		sprungZeitAlt = 0;
+		sprungSchalter = true;
+		
+        //Initialisierung der Rotationsdaten in Form von Quaternions der jeweiligen Kameras
+        //Ausgangslage
         originalDrehOben = kameraOben.transform.rotation;
         originalDrehVorne = kameraVorne.transform.rotation;
 		originalDrehHinten = kameraHinten.transform.rotation;
         originalDrehUnten = kameraUnten.transform.rotation;
-		originalDrehLinks = kameraLinks.transform.rotation;
-        originalDrehRechts = kameraRechts.transform.rotation; 
-        
+	    
+		//alle Kollisionsvariablen auf false, zu Beginn keine Kollision        
         drehOV = false;
         drehOH = false;
     	drehOL = false;
@@ -130,35 +110,20 @@ public class SpielerInput	: MonoBehaviour {
     	drehHR = false;
     	drehHL = false;
     	
+    	//zu Beginn des Spiels startet Spieler oben auf dem Würfel, somit Kamera oben aktiviert, 
+    	//andere Kameras deaktiviert
 		kameraOben.enabled = true;
 		kameraUnten.enabled = false;
- 		kameraLinks.enabled = false;
-		kameraRechts.enabled = false;
-		kameraHinten.enabled = false;
+ 		kameraHinten.enabled = false;
 		kameraVorne.enabled = false;
 		
-		drehKin = GetComponent<Rigidbody>();
-    	
-//        zeitGesamt = 3f;
-//        winkelGesamt = 90f;
-//        zeitAnteilAlt = 0;
 	}
 	
-//	void Springen(){
-//		
-//			
-//		jump += 1.5f;
-//		
-//		transform.position += transform.up * Mathf.Sin(jump);
-//		
-//		if( jump <= Mathf.PI){
-//				
-//			jumping = false;
-//			jump = 0;
-//			
-//		}
-//	}
-	
+
+	// Abfangen des Triggers der jeweiligen Plane, welche durchstoßen wurde, beim Austreten aus der Plane ausgeführt
+	// bei Oben, Unten, Vorne, Hinten : aktivieren der jeweiligen Funktion in der betreffenden Klasse der Kamera und 
+	//									Übergabe/Speicherung der Startzeit der Animation
+	// bei Links, Rechts : Aktivieren der jeweiligen Links- oder Rechtsbewegung, damit der Spieler wieder steuern kann
 	void OnTriggerExit(Collider coll){
 		
 		if ( coll.gameObject.tag == "PlaneObenVorne")
@@ -181,9 +146,7 @@ public class SpielerInput	: MonoBehaviour {
 		{
 			drehOR = true;
 			
-			sperreRechts = true;
-			
-			//CameraRechtsSteuerung.drehRechts = true;	
+			sperreRechts = true;	
 		}
 		
 		if ( coll.gameObject.tag == "PlaneObenLinks")
@@ -206,21 +169,20 @@ public class SpielerInput	: MonoBehaviour {
 			
 			sperreRechts = true;
 			
-			//CameraRechtsSteuerung.drehVR = true;
 		}
 		
 		if ( coll.gameObject.tag == "PlaneHintenLinks")
 		{
 			drehHL = true;
 			
-			sperreLinks = true;
+			sperreRechts = true;
 		}
 		
 		if ( coll.gameObject.tag == "PlaneHintenRechts")
 		{
 			drehHR = true;
 			
-			sperreRechts = true;
+			sperreLinks = true;
 		}
 		
 		if ( coll.gameObject.tag == "PlaneUntenVorne")
@@ -254,170 +216,89 @@ public class SpielerInput	: MonoBehaviour {
 		
 	}
 	
+	
+	
+	// Abfangen des Triggers der jeweiligen Plane, welche gerade durchstoßen wird 
+	// bei Links, Rechts : Deaktivieren der jeweiligen Links- oder Rechtsbewegung, damit der Spieler nicht steuern kann
 	void OnTriggerStay(Collider coll){
 		
-		if ( coll.gameObject.tag == "PlaneObenVorne")
-		{
-			drehOV = true;
-			
-			bewegungZeitStart = Time.time;
-			
-		}
-		
-		if ( coll.gameObject.tag == "PlaneObenHinten")
-		{
-			drehOH = true;
-			
-			bewegungZeitStart = Time.time;
-			
-		}
 		
 		if ( coll.gameObject.tag == "PlaneObenRechts")
 		{
-			drehOR = true;
-			
-			sperreRechts = false;
-			
-			//CameraRechtsSteuerung.drehRechts = true;	
+			sperreRechts = false;	
 		}
 		
 		if ( coll.gameObject.tag == "PlaneObenLinks")
 		{
-			drehOL = true;
-			
 			sperreLinks = false;
 		}
 		
 		if ( coll.gameObject.tag == "PlaneVorneLinks")
 		{
-			drehVL = true;
-			
 			sperreLinks = false;
 		}
 		
 		if ( coll.gameObject.tag == "PlaneVorneRechts")
 		{
-			drehVR = true;
-			
-			sperreRechts = false;
-			
-			//CameraRechtsSteuerung.drehVR = true;
+			sperreRechts = false;			
 		}
 		
 		if ( coll.gameObject.tag == "PlaneHintenLinks")
 		{
-			drehHL = true;
-			
-			sperreLinks = false;
+			sperreRechts = false;
 		}
 		
 		if ( coll.gameObject.tag == "PlaneHintenRechts")
 		{
-			drehHR = true;
-			
-			sperreRechts = false;
-		}
-		
-		if ( coll.gameObject.tag == "PlaneUntenVorne")
-		{
-			drehUV = true;
-			
-			bewegungZeitStart = Time.time;
-			
-		}
-		
-		if ( coll.gameObject.tag == "PlaneUntenHinten")
-		{
-			drehUH = true;
-			
-			bewegungZeitStart = Time.time;
+			sperreLinks = false;
 		}
 		
 		if ( coll.gameObject.tag == "PlaneUntenRechts")
 		{
-			drehUR = true;
-			
 			sperreRechts = false;
 		}
 		
 		if ( coll.gameObject.tag == "PlaneUntenLinks")
 		{
-			drehUL = true;
-			
 			sperreLinks = false;
 		}
 		
 	}
 	
-//	void OnCollisionEnter(Collision coll){
-//		
-//		if ( coll.gameObject.tag == "PlaneVorneOben")
-//		{
-//			drehX = true;
-//			
-//			
-//
-//		}
-//		
-//	}
-
 	
-	// Update is called once per frame
 	void Update () { 
 
-//		if (CameraRechtsSteuerung.drehRechts){
-//			
-//			targetPos = -(transform.right * (5f*step));
-//			
-//		}
-//		
-	// Spieler bei Tastenbedienung A Bewegung nach links
+
+	// Spieler bei Tastenbedienung A Bewegung nach links, wenn Sperre inaktiv
 	if( Input.GetKeyDown( KeyCode.A ) && sperreLinks){
 		
 		targetPos = -(transform.right * step);
 		
 	}
 	
-	
-//	if (CameraRechtsSteuerung.drehRechts){
-//		
-//		drehKin.isKinematic = false;
-//		
-//		GetComponent<Rigidbody>().AddTorque(0, 45f , -90f);
-//		drehKin.isKinematic = true;
-//		
-//	}
-	
-//	if (CameraRechtsSteuerung.drehVR){
-//		
-//		//GetComponent<Rigidbody>().AddTorque(0, -90f, 45f);
-//		
-//	}
-//	
-	
-	
-	//Spieler bei Tastenbedienung D Bewegung nach rechts
+	//Spieler bei Tastenbedienung D Bewegung nach rechts, wenn Sperre inaktiv
 	if( Input.GetKeyDown( KeyCode.D ) && sperreRechts ){
 		
 		targetPos = transform.right * step;
 		
 	}	
 	
+	//nur springen ermöglichen wenn keine Sperre drin
 	if (sprungSchalter){
 	
-	//Spieler bei Tastenbedienung Leertaste Bewegung in einer Parabel über das Hindernis
-	if  (Input.GetKeyDown( KeyCode.Space ) && !jumping){
-				
-		jumping = true;
-		
-		zeitSchaltuhrStart = Time.time;
-		
-	}
-	
+		//Spieler bei Tastenbedienung Leertaste Bewegung in einer Parabel über das Hindernis
+		if  (Input.GetKeyDown( KeyCode.Space ) && !jumping){
+					
+			jumping = true;
+			
+			zeitSchaltuhrStart = Time.time;
+			
+		}
 	}
 	
 	else {
 		
+		//Zeit stoppen, Sperre wieder raus und dann kann er wieder springen (Stoppuhr)		
 		float zeitSperre = 1.5f;
 		float vergangen = Time.time - zeitSchaltuhrStart;
 			
@@ -428,6 +309,7 @@ public class SpielerInput	: MonoBehaviour {
 	}
 
 	
+	//Sprungfunktion selber
 	if (jumping){
 		
 		sprungSchalter = false;
@@ -451,42 +333,8 @@ public class SpielerInput	: MonoBehaviour {
 			sprungZeitAlt = 0;
 			sprungSchalter = false;
 			
-		}
-		
+		}	
 	}
-		
-//	else if( Input.GetKeyDown( KeyCode.H ) && !drehX){
-//		
-//		Debug.Log("Ente in Platte");
-//		
-//		drehX = true;
-//
-//		bewegungZeitStart = Time.time;
-//		
-//	}
-	
-	
-	
-//	if(drehX){
-////		
-//		bewegungZeitStart = Time.time;
-//		
-//		zeitAnteil = (Time.time - bewegungZeitStart) / zeitGesamt;
-////		
-//		winkelAenderung = (zeitAnteil - zeitAnteilAlt) * winkelGesamt;
-////		
-////		kamera.transform.RotateAround( new Vector3(0, 10f, -10f), new Vector3(1f, 0, 0), winkelAenderung );
-////		
-//		zeitAnteilAlt = zeitAnteil;
-////		
-////		Debug.Log(kamera.transform.eulerAngles.x);
-////		
-//		if(zeitAnteil >= 1f){
-//			drehX = false;
-//		}
-////		
-//	}
-////	
 	
 	//Bewegungen nach links und rechts abdämpfen mit smoothing
 	transform.position += targetPos * smoothing;
